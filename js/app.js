e7d5722c2a2559f53d65dfb8ff1055afe228c056
Priyank Thakkar <priyankthakkar@Priyanks-MacBook-Air.local>
2016-12-12 22:02:19 +0530

    Change Log: 1. Added form validation support with Angular auto validate and custom validation messages 2. Cleaned up from a bit, removie pre tag Reviewed and Committed By: Priyank Thakkar

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
-var app = angular.module('minmax', []);
+var app = angular.module('minmax', [
+  'jcs-autoValidate'
+]);
+
+app.run(function(defaultErrorMessageResolver){
+  defaultErrorMessageResolver.getErrorMessages().then(function(errorMessages){
+    errorMessages['tooYoung'] = 'You must be atleast {0} years old to use this site.';
+    errorMessages['tooOld'] = 'You must max {0} years to use this site.';
+    errorMessages['badUsername'] ="Username can container only characters and numbers and _";
+  });
+});
 
 app.controller('MinMaxController', function($scope, $http){
   $scope.formModel = {};
 
   $scope.onSubmit = function(){
+    console.log('I am submitted');
+
     $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
       then(function(data){
         console.log(':)');
